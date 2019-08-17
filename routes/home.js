be7a2d6daf2689481f8b1c73ac45604eb892ec09
Yijun He <yijunhhhe@gmail.com>
2019-08-17 17:00:50 -0400

    update

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render("index.html")
 });
 
+router.get('/login', function(req, res, next) {
+  res.render("login.html")
+});
+
+router.get('/register', function(req, res, next) {
+  res.render("register.html")
+});
 module.exports = router;
